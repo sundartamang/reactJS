1b7e69e54ec267783285d3fc4f5225ea301fe9b5
sursasundar <sundar.tamang@sursatech.com>
2024-08-14 16:02:24 +0545

    feat: defination added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import './App.css';
 import { useMemo, useState } from 'react';
-
 
 const calculate = () => {
   let result = 0;
   for (let i = 0; i < 10000; i++) {
-    console.log("i ", i)
     result += 10;
   }
   return result;
-}
+};
 
 const App = () => {
   const [count, setCount] = useState(0);
-
   const [dependentCount, setDependentCount] = useState(0);
+  
   useMemo(calculate, [dependentCount]);
 
   return (
@@ -23,10 +20,12 @@ const App = () => {
       <h5>Count : {count}</h5>
 
       <br />
-      <button onClick={() => setDependentCount(dependentCount + 1)}>Increment 2</button>
-      <h5>Depndent Count : {dependentCount}</h5>
+      <button onClick={() => setDependentCount(dependentCount + 0)}>Increment 2</button>
+      <h5>Dependent Count : {dependentCount}</h5>
     </div>
   );
-}
+};
 
 export default App;
+
+
